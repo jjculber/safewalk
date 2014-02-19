dd29c6865fb81d93a3c5b943a20699f38fa6f6fb
Justin Culbertson <justin@repairtechsolutions.com>
2014-02-19 14:47:32 -0800

    Hopefully making User serializable.

--- src/main/java/com/polysafewalk/model/User.java
+++ src/main/java/com/polysafewalk/model/User.java
@@ -47,13 +47,21 @@ public class User {
 	public void setConfirmKey(String confirmKey) {
 		this.confirmKey = confirmKey;
 	}
-	
+
 	public boolean isActive() {
 		return this.getConfirmKey() == null || this.getConfirmKey().isEmpty();
 	}
-	
+
 	public String getFullName() {
 		return this.getFirstName() + " " + this.getLastName();
+	}
+
+	public void setActive(boolean ignored) {
+		return;
+	}
+
+	public void setFullName(String ignored) {
+		return;
 	}
 
 }
