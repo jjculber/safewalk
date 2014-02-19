a615d4f96c3c31b95cd2ddbb484c2a088d2fd14f
Justin Culbertson <justin@repairtechsolutions.com>
2014-02-19 14:24:52 -0800

    Added email templates.

--- src/main/java/com/polysafewalk/controller/UsersController.java
+++ src/main/java/com/polysafewalk/controller/UsersController.java
@@ -1,6 +1,8 @@
 package com.polysafewalk.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.mail.MessagingException;
@@ -10,12 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +32,8 @@ import com.polysafewalk.model.User;
 import com.polysafewalk.service.LogService;
 import com.polysafewalk.service.NotificationService;
 import com.polysafewalk.service.UserService;
+
+import org.apache.velocity.app.VelocityEngine;
 
 @Controller
 public class UsersController {
@@ -43,6 +49,9 @@ public class UsersController {
 
 	@Autowired
 	private JavaMailSender mailSender;
+
+	@Autowired
+	private VelocityEngine velocityEngine;
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public String registerUser(UserRegistrationForm userRegistrationForm,
@@ -60,28 +69,17 @@ public class UsersController {
 			String password = userRegistrationForm.getPassword();
 			doAutoLogin(email, password, request);
 
-			MimeMessage mimeMessage = mailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,
-					false, "utf-8");
-			String confirmUrl = "http://www.polysafewalk.com/confirm/"
-					+ confirmKey;
-			String htmlMsg = "Thank you for signing up!<br/><br/>"
-					+ "Please confirm your email address:<br/>" + "<a href=\""
-					+ confirmUrl + "\">" + confirmUrl + "</a><br/><br/>Thanks!";
-			mimeMessage.setContent(htmlMsg, "text/html");
-			helper.setTo(email);
-			helper.setSubject("Thanks for signing up for PolySafeWalk!");
-			helper.setFrom("info@polysafewalk.com");
-			mailSender.send(mimeMessage);
+			User user = (User) SecurityContextHolder.getContext()
+					.getAuthentication().getPrincipal();
+			user.setConfirmKey(confirmKey);
+			
+			sendConfirmationEmail(user);
 
 			return "redirect:/home";
 
 		} catch (UsernameNotUniqueException e) {
 			return "redirect:/?error=Email%20already%20signed%20up";
-		} catch (MessagingException e) {
-			e.printStackTrace();
 		}
-		return "redirect:/home";
 	}
 
 	@RequestMapping("/confirm/{confirmKey}")
@@ -136,21 +134,46 @@ public class UsersController {
 		}
 	}
 
-	private void sendReminderEmail(Notification notification, User user) {
-		try {
-			MimeMessage mimeMessage = mailSender.createMimeMessage();
-			MimeMessageHelper helper;
-			helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
-			String htmlMsg = "Your walk is scheduled to leave in less than 10 minutes."
-					+ "<br/><br/>Thanks,<br/>PolySafeWalk";
-			mimeMessage.setContent(htmlMsg, "text/html");
-			helper.setTo(user.getEmail());
-			helper.setSubject("Walk Reminder");
-			helper.setFrom("info@polysafewalk.com");
-			mailSender.send(mimeMessage);
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
+	private void sendReminderEmail(final Notification notification,
+			final User user) {
+		MimeMessagePreparator preparator = new MimeMessagePreparator() {
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+				message.setTo(user.getEmail());
+				message.setFrom("info@polysafewalk.com");
+				message.setSubject("Walk Reminder");
+
+				Map<String, Object> model = new HashMap<String, Object>();
+				model.put("user", user);
+
+				String text = VelocityEngineUtils.mergeTemplateIntoString(
+						velocityEngine, "walk-reminder.vm",
+						model);
+				message.setText(text, true);
+			}
+		};
+		this.mailSender.send(preparator);
+	}
+
+	private void sendConfirmationEmail(final User user) {
+		MimeMessagePreparator preparator = new MimeMessagePreparator() {
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+				message.setTo(user.getEmail());
+				message.setFrom("info@polysafewalk.com");
+				message.setSubject("Thanks for signing up for PolySafeWalk!");
+
+				Map<String, Object> model = new HashMap<String, Object>();
+				model.put("user", user);
+				model.put("confirmKey", user.getConfirmKey());
+
+				String text = VelocityEngineUtils.mergeTemplateIntoString(
+						velocityEngine, "registration-confirmation.vm",
+						model);
+				message.setText(text, true);
+			}
+		};
+		this.mailSender.send(preparator);
 	}
 
 }
